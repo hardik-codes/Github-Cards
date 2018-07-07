79b4405360e60a072f2bf190b1b178b1367bc6da
Hardik Agarwal <hardik06agarwal@gmail.com>
2018-07-07 20:54:28 +0530

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile';
+import Quest from './Quest';
+
 
 class App extends Component {
+    state = {
+      username: 'hardik-codes',
+      webAddress: '',
+      name: '',
+      place: '',
+      repos: '',
+      followers: '',
+      pic: '',
+      notFound: '',
+      following:''
+      
+    }
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <a href="https://github.com/hardik-codes/forum"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
+alt="Fork me on GitHub"/>
+      </a>
+        <Profile info={this.state} />
+        <Quest getUser={this.getUser.bind(this)}/>
       </div>
     );
   }
+  
+  
+  fetchDataFromAPI(link) {  
+    
+    fetch(link)
+      .then((response) => response.json() )
+      .then((userInfo) => {
+        
+        
+        this.setState({
+          username: userInfo.login,
+          webAddress: userInfo.html_url,
+          name: userInfo.name,
+          place: userInfo.location,
+          repos: userInfo.public_repos,
+          followers: userInfo.followers,
+          pic: userInfo.avatar_url,
+          notFound: userInfo.message,
+          following:userInfo.following
+         
+        })
+      })
+      .catch((error) => console.log('There is some problem!') )
+  }
+  
+  getUser(username) { 
+    this.fetchDataFromAPI(`https://api.github.com/users/${username}`)
+  }
+  
+  componentDidMount() {     
+    let url = `https://api.github.com/users/${this.state.username}`
+    this.fetchDataFromAPI(url)
+  } 
 }
+
 
 export default App;
