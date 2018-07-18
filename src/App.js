66c67b628e659aea597b2696af833e645436710b
Hardik Agarwal <hardik06agarwal@gmail.com>
2018-07-18 23:14:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,9 +21,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <a href="https://github.com/hardik-codes/Github-Cards"><img src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
--alt="Fork me on GitHub"/>
--      </a>
         <Profile info={this.state} />
         <Quest getUser={this.getUser.bind(this)}/>
       </div>
